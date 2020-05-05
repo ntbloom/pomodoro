8a93b5aab6ae7a345233f480d45eaedcd988d09d
ntbloom <ntbloom@gmail.com>
2020-05-05 16:57:03 -0400

    wrote schema and connected to database

--- backend/create_database.py
+++ backend/create_database.py
@@ -1,9 +1,28 @@
 import sqlite3
 from pathlib import Path
 
+
 class Dbconnector:
     def __init__(self, database: Path):
         self.conn = sqlite3.connect(database=database)
         self.cursor = self.conn.cursor()
+        self._create_schema()
 
-
+    def _create_schema(self):
+        script = """
+                BEGIN TRANSACTION;
+                DROP TABLE IF EXISTS tasks;
+                CREATE TABLE tasks (
+                    taskid TEXT PRIMARY KEY,
+                    task TEXT NOT NULL,
+                    desc TEXT NOT NULL 
+                );
+                DROP TABLE IF EXISTS tasklog;
+                CREATE TABLE tasklog (
+                    taskid TEXT,
+                    finished INTEGER, -- 0 = false, 1 = true
+                    FOREIGN KEY(taskid) REFERENCES tasks(taskid)
+                );
+                COMMIT TRANSACTION;
+        """
+        self.cursor.executescript(script)
--- backend/tests/database_tests.py
+++ backend/tests/database_tests.py
@@ -1,12 +1,25 @@
-import pytest
+"""
+setup for general test-driven development. incremental tests assure code has 100% coverage.
+run test suite `python -m pytest database_tests.py`
+"""
+
 from pathlib import Path
 from backend.create_database import Dbconnector
 
 DATABASE = Path("../pomodoro.db")
+DB = Dbconnector(DATABASE)
 
 
 class TestDatabaseFunctionality:
     def test_connection(self):
-        d = Dbconnector(DATABASE)
-        result = d.cursor.execute("""SELECT 1+1;""").fetchone()[0]
+        result = DB.cursor.execute("""SELECT 1+1;""").fetchone()[0]
         assert result == 2
+
+    def test_schema_load(self):
+        tasks = DB.cursor.execute("""SELECT * FROM tasks;""").fetchone()
+        tasklog = DB.cursor.execute("""SELECT * FROM tasklog;""").fetchone()
+        assert True  # will error if table doesn't exist
+
+    def test_insert_data(self):
+        # will not continue TDD for the rest of the database
+        pass
